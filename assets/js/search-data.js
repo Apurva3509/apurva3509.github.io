0a2dc09afa6f08abcb83319004b327b3f08a6819
Apurva3509 <145287094+Apurva3509@users.noreply.github.com>
2025-01-15 04:29:36 +0000

    Deploying to gh-pages from @ Apurva3509/apurva3509.github.io@1ad8d05a8da91ed289b94036d678d6817fb9915d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,7 +4,7 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about-apurva",
-    title: "About Apurva",
+    title: "About APurva",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
